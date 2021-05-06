96ba03ad18870d72e81f2baa2c4b28f3c2971829
Joel Wembo <joelotepawembo@gmail.com>
2021-05-07 00:16:54 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,6 +9,32 @@ let a_middleware_function = function(req, res, next) {
   next(); // Call next() so Express will call the next middleware function in the chain.
 }
 
+//this works with older versions of  mongodb version ~ 2.2.33
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/animals', function(err, db) {
+  if (err) throw err;
+
+  db.collection('mammals').find().toArray(function (err, result) {
+    if (err) throw err;
+
+    console.log(result);
+  });
+});
+
+//for mongodb version 3.0 and up
+const MongoClient = require('mongodb').MongoClient;
+MongoClient.connect('mongodb://localhost:27017/animals', function(err, client){
+   if(err) throw err;
+
+   let db = client.db('animals');
+   db.collection('mammals').find().toArray(function(err, result){
+     if(err) throw err;
+     console.log(result);
+     client.close();
+   });
+});
+
 // Function added with use() for all routes and verbs
 app.use(a_middleware_function);
 
